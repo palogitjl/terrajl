fc71ebcd52fdda097df0d58fdc3c3c47da218802
palogitjl <91766267+palogitjl@users.noreply.github.com>
2022-06-27 13:49:41 -0400

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -115,7 +115,7 @@ resource "aws_s3_bucket" "logs" {
   bucket = "${local.resource_prefix.value}-logs"
   acl    = "log-delivery-write"
   versioning {
-    enabled = true
+    enabled = false
   }
   server_side_encryption_configuration {
     rule {
